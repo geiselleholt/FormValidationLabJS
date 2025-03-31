d76006e9d8c9b8d6eb3fc31ff67dd6d66037c10c
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-30 21:14:30 -0600

    Part 3 #6 local storage

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 let regForm = document.getElementById("registration");
-
 regForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
-  e.preventDefault();
-  let password = document.getElementById("password").value;
-  let passwordCheck = document.getElementById("passwordCheck").value;
-  let username = document.getElementById("username").value;
-  //   console.log(password, passwordCheck, username);
+//   e.preventDefault();
+  let passwordValue = document.getElementById("password").value;
+  let passwordCheck = document.getElementById("passwordCheck");
+  let passwordCheckValue = document.getElementById("passwordCheck").value;
+  let username = document.getElementById("username");
 
-  if (password !== passwordCheck) {
+  let usernameValue = document.getElementById("username").value;
+
+  if (passwordValue !== passwordCheckValue) {
     alert("that don't match");
     passwordCheck.focus();
-    return false;
+    return;
   }
 
-  if (password.includes(username)) {
-    password.focus();
-    return false;
+  if (passwordValue.includes(usernameValue)) {
+    alert("password can't contain usersname");
+    username.focus();
+    return;
   }
+
+  let allData = [];
+
+  localStorage.setItem("password", passwordValue);
+  let storedPassword = localStorage.getItem("password");
+
+  localStorage.setItem("username", usernameValue.toLowerCase());
+  let storedUsername = localStorage.getItem("username");
+
+  let emailValue = document.getElementById("email").value;
+  localStorage.setItem("email", emailValue.toLowerCase());
+  let storedEmail = localStorage.getItem("email");
+
+  allData.forEach((data) => {
+    if (data.username == usernameValue) {
+      alert("that username is already taken");
+      username.focus();
+      return;
+    }
+  });
+
+  let data = {
+    username: storedUsername,
+    password: storedPassword,
+    email: storedEmail,
+  };
+  allData.push(data);
+
+  localStorage.clear();
+  alert("submit successful!");
 }
