ad0f527c269329163540c6297ddaab6b3aa8b6fb
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-30 19:19:58 -0600

    Part 3 done except password matching

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-let regForm = document.getElementById('registration')
-let regEmail = document.getElementById('email')
+let regForm = document.getElementById("registration");
+let password = document.getElementById("password");
+let passwordCheck = document.getElementById("passwordCheck");
+let username = document.getElementById("username");
+let terms = document.getElementById("terms");
+let passwordValue = password.value;
+let passwordCheckValue = passwordCheck.value;
+let usernameValue = username.value;
 
-// regForm.addEventListener('submit', handleRegForm)
+regForm.addEventListener("submit", handleSubmit);
 
-// function regForm(e) {
-//     if (regEmail.endwWith) { }
+function handleSubmit(e) {
+  e.preventdefault();
+  if (passwordValue !== passwordCheckValue) {
+    return false;
+  }
 
-// }
+  if (password.includes(usernameValue)) {
+    return false;
+  }
+}
